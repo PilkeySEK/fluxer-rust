117b8dc911b6e7b4226f59823c1b62eb6ca8dc56
PilkeySEK <PilkeySEK@gmx.de>
2026-05-26 10:02:35 +0200

    user in GuildMemberRemove is not PartialUser

--- neptunium-model/src/gateway/payload/incoming/members_events/guild_member_remove.rs
+++ neptunium-model/src/gateway/payload/incoming/members_events/guild_member_remove.rs
@@ -1,12 +1,17 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    id::{Id, marker::GuildMarker},
-    user::PartialUser,
+use crate::id::{
+    Id,
+    marker::{GuildMarker, UserMarker},
 };
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
+pub struct GuildMemberRemoveMember {
+    pub id: Id<UserMarker>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub struct GuildMemberRemove {
     pub guild_id: Id<GuildMarker>,
-    pub user: PartialUser,
+    pub user: GuildMemberRemoveMember,
 }
